bf79ec0150ec41814bfc122677afdfbe30ffcfdb
Dylan <dylan.7mcdougall@gmail.com>
2023-06-08 16:58:31 -0700

    created skeleton for all models and migrations

---
+++ backend/db/models/event.js
@@ -0,0 +1,43 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Event extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Event.init({
+    groupId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    venueId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    type: DataTypes.STRING,
+    capacity: DataTypes.INTEGER,
+    price: DataTypes.NUMERIC,
+    startDate: DataTypes.DATE,
+    endDate: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Event',
+  });
+  return Event;
+};
