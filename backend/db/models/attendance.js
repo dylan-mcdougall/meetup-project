a9b2f1b7e6b75ac3304399e16f4160e2cf9e7b62
Dylan <dylan.7mcdougall@gmail.com>
2023-06-15 17:46:49 -0700

    maybe this time

--- backend/db/models/attendance.js
+++ backend/db/models/attendance.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Attendance.belongsTo(
         models.Event,
-        { foreignKey: 'eventId' }
+        { foreignKey: 'eventId', targetKey: 'id' }
       );
       Attendance.belongsTo(
         models.User,
