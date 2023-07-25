16ae34428ae9629d67e50a1cb84c1b2fa5ccdc49
Dylan <dylan.7mcdougall@gmail.com>
2023-07-24 17:09:04 -0700

    set styling and baseline functionalities for landing page

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -2,38 +2,52 @@ import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
+import './HomePage.css';
 
 function HomePage() {
+    const sessionUser = useSelector(state => state.session.user);
 
+    const h3ClassName = 'Section-3-title-3' + (sessionUser ? "" : ' unauthorized');
 
     return (
-        <>
-            <div className="Section-1">
-                <h2 className="Section-1-title">The people platform-
-                    Where interests 
-                    become friendships
-                </h2>
-                <p className="Section-1-paragraph"></p>
+        <div className="Landing-wrapper">
+            <div className='Section-1'>
+                <div className="Section-1-text">
+                    <h2 className="Section-1-title">The people platform--
+                        Where interests
+                        become friendships
+                    </h2>
+                    <p className="Section-1-paragraph">Your new community is waiting for you. For 20+ years, millions of people have chosen Meetup to make real connections over shared interests. Start a group today with a 30-day free trial.</p>
+                </div>
+                <div className="Section-1-images">
+                    <p>placeholder for image</p>
+                </div>
             </div>
             <div className="Section-2">
-                <h2 className="Section-2-title">beep</h2>
-                <p className="Section-2-paragraph"></p>
+                <h2 className="Section-2-title">How Meetup works</h2>
+                <p className="Section-2-paragraph">Spark new friendships. Start a group to meet people who are new in town or longtime locals. Enjoy nightlife, happy hours, game nights, music, and more.</p>
             </div>
             <div className="Section-3">
                 <div className="Section-3-column-1">
-
+                    <h3 className="Section-3-title-1">
+                        <a href='#'>See all groups</a>
+                    </h3>
                 </div>
                 <div className="Section-3-column-2">
-
+                <h3 className="Section-3-title-2">
+                        <a href='#'>Find an event</a>
+                    </h3>
                 </div>
                 <div className="Section-3-column-3">
-
+                    <h3 className={h3ClassName}>
+                        <a href='#'>Start a new group</a>
+                    </h3>
                 </div>
             </div>
             <div className="Section-4">
                 <button className="Section-4-button">Join Meetup</button>
             </div>
-        </>
+        </div>
     )
 }
 
